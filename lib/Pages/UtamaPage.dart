5bd76483b4b1a418016bfe6e1e96738d1331ba33
M2NV <m2sjmail@gmail.com>
2025-01-12 20:21:48 +0700

    a

--- lib/Pages/UtamaPage.dart
+++ lib/Pages/UtamaPage.dart
@@ -1,2 +1,15 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+
+class CRUDutama extends StatelessWidget {
+  const CRUDutama({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+
+      ),
+    );
+  }
+}
