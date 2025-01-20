8da48a1d06678288c64cae8d8870dc0ebb120d24
M2NV <m2sjmail@gmail.com>
2025-01-20 09:23:47 +0700

    FIX [BW] wrong routing fix

---
+++ lib/Pages/Fragments/CompletedTask.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+
+class CompletedtaskPage extends StatelessWidget {
+  const CompletedtaskPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
